2f87d2757b7948ffac27c6adaf1c4c668487aa3c
zonda yang <u226699@gmail.com>
2021-10-07 00:07:11 +0800

    Customize error message

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::io::prelude::*;
 use std::io::Result;
 use std::net::TcpStream;
+use std::error::Error;
 
 fn main() -> Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8000")?;
+    let mut stream = TcpStream::connect("127.0.0.1:8000")
+                                .expect("Couldn't connect to the server..");
     let mut buffer = [0; 1024];
 
-    stream.write(&[1])?;
-    stream.read(&mut buffer)?;
+    stream.write(&[1]).expect("Couldn't send data to server...");
+    stream.read(&mut buffer).expect("Couldn't recv data from server...");
     println!("{:?}", buffer);
     Ok(())
 }

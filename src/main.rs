8a6d0c970f585fca17a95e7cf008604ce4349139
zonda yang <u226699@gmail.com>
2021-10-07 23:00:00 +0800

    Add resolve function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use std::io::prelude::*;
 use std::io::Result;
-use std::net::TcpStream;
+use std::net::{TcpStream, ToSocketAddrs, SocketAddr};
 use std::error::Error;
 
+fn resolve(domain: &str) -> Vec<SocketAddr> {
+   domain.to_socket_addrs()
+       .expect("Unable to resolve domain")
+       .collect()
+}
+
 fn main() -> Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8000")
+    let target = "google.com:80";
+
+    let server = resolve(target);
+    println!("{:?}", server);
+
+    let mut stream = TcpStream::connect(target)
                                 .expect("Couldn't connect to the server..");
     let mut buffer = [0; 1024];
 
+    println!("{:?}", stream.peer_addr().unwrap());
     stream.write(&[1]).expect("Couldn't send data to server...");
     stream.read(&mut buffer).expect("Couldn't recv data from server...");
     println!("{:?}", buffer);

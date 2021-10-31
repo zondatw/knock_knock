3443557d8f2e3a697e95c5b9ce0851a9b736eef6
zonda yang <u226699@gmail.com>
2021-11-01 00:07:46 +0800

    Separate uri

--- pinger/src/lib.rs
+++ pinger/src/lib.rs
@@ -4,7 +4,8 @@ use std::io::Result;
 use std::io::{Error, ErrorKind};
 use std::net::{SocketAddr, TcpStream, ToSocketAddrs, UdpSocket};
 use std::time::{Duration, Instant};
-use regex::Regex;
+
+mod uri;
 
 #[path = "tests/test_pinger.rs"]
 #[cfg(test)]
@@ -13,130 +14,16 @@ mod test_pinger;
 const BUF_SIZE: usize = 0xFF;
 const HTTP_UNCONNECT_STATUS_CODE: &'static [&'static str] = &["404", "501"];
 
-pub struct URI {
-    scheme: String,
-    username: String,
-    password: String,
-    host: String,
-    domain: String,
-    port: i32,
-    path: String,
-    query: String,
-    fragment: String,
-}
-
-impl URI {
-    fn default() -> URI {
-        URI {
-            scheme: String::from(""),
-            username: String::from(""),
-            password: String::from(""),
-            host: String::from(""),
-            domain: String::from(""),
-            port: 0,
-            path: String::from("/"),
-            query: String::from(""),
-            fragment: String::from(""),
-        }
-    }
-
-    fn parse(&mut self, url: &str) -> Result<()> {
-        let rg_w_named = Regex::new(r"^((?P<scheme>[^:/?#]+)://)?((?P<username>\w+):(?P<password>\w+)@)?(?P<host>(?P<domain>[^/?#]*)(:(?P<port>\d*)))?(?P<path>[^?#]*)(\?(?P<query>[^#]*))?(#(?P<fragment>.*))?").unwrap(); match rg_w_named.captures(url) {
-            Some(uri_parser) => {
-                match uri_parser.name("scheme") {
-                    Some(v) => self.scheme = v.as_str().to_string(),
-                    None => self.scheme = String::from(""),
-                }
-                match uri_parser.name("host") {
-                    Some(v) => self.host = v.as_str().to_string(),
-                    None => self.host = String::from(""),
-                }
-                match uri_parser.name("username") {
-                    Some(v) => self.username = v.as_str().to_string(),
-                    None => self.username = String::from(""),
-                }
-                match uri_parser.name("password") {
-                    Some(v) => self.password = v.as_str().to_string(),
-                    None => self.password = String::from(""),
-                }
-                match uri_parser.name("domain") {
-                    Some(v) => self.domain = v.as_str().to_string(),
-                    None => self.domain = String::from(""),
-                }
-                match uri_parser.name("port") {
-                    Some(v) => self.port = v.as_str().to_string().parse::<i32>().unwrap(),
-                    None => self.port = 0,
-                }
-                match uri_parser.name("path") {
-                    Some(v) => self.path = v.as_str().to_string(),
-                    None => self.path = String::from("/"),
-                }
-                match uri_parser.name("query") {
-                    Some(v) => self.query = v.as_str().to_string(),
-                    None => self.query = String::from(""),
-                }
-                match uri_parser.name("fragment") {
-                    Some(v) => self.fragment = v.as_str().to_string(),
-                    None => self.fragment = String::from(""),
-                }
-            },
-            None => return Result::Err(Error::new(ErrorKind::InvalidData, "Invalid url")),
-        }
-        Ok(())
-    }
-
-    fn get_url(&mut self) -> String {
-        format!(
-            "{}://{}:{}@{}{}{}#{}",
-            self.scheme,
-            self.username,
-            self.password,
-            self.host,
-            self.path,
-            self.query,
-            self.fragment,
-        )
-    }
-
-    fn display(&mut self) {
-        println!("URI:\n\
-             \tScheme: {:?}\n\
-             \tUsername: {:?}\n\
-             \tPassword: {:?}\n\
-             \tHost: {:?}\n\
-             \tDomain: {:?}\n\
-             \tPort: {:?}\n\
-             \tPath: {:?}\n\
-             \tQuery: {:?}\n\
-             \tFragment: {:?}\n",
-             self.scheme,
-             self.username,
-             self.password,
-             self.host,
-             self.domain,
-             self.port,
-             self.path,
-             self.query,
-             self.fragment,
-            );
-    }
-}
-
-pub fn get_uri(url: &str) -> URI {
-    let mut uri = URI::default();
-    uri.parse(url);
-    uri
-}
 
 fn get_host_path(url: &str) -> String {
     // let s: Vec<&str> = url.split("/").collect();
     // s[0]
-    let uri = get_uri(url);
+    let uri = uri::get_uri(url);
     uri.host
 }
 
 pub fn resolve(url: &str) -> Vec<SocketAddr> {
-    let uri = get_uri(url);
+    let uri = uri::get_uri(url);
     uri.host.as_str()
         .to_socket_addrs()
         .expect("Unable to resolve domain")
@@ -215,7 +102,7 @@ fn httping(target: &str, body: String) -> Result<()> {
 }
 
 pub fn httping_connect(target: &str) -> Result<()> {
-    let uri = get_uri(target);
+    let uri = uri::get_uri(target);
     httping(
         target,
         format!(
@@ -231,7 +118,7 @@ pub fn httping_connect(target: &str) -> Result<()> {
 }
 
 pub fn httping_get(target: &str) -> Result<()> {
-    let uri = get_uri(target);
+    let uri = uri::get_uri(target);
     httping(
         target,
         format!(
@@ -248,7 +135,7 @@ pub fn httping_get(target: &str) -> Result<()> {
 }
 
 pub fn httping_post(target: &str) -> Result<()> {
-    let uri = get_uri(target);
+    let uri = uri::get_uri(target);
     httping(
         target,
         format!(
@@ -269,7 +156,7 @@ pub fn httping_post(target: &str) -> Result<()> {
 }
 
 pub fn httping_put(target: &str) -> Result<()> {
-    let uri = get_uri(target);
+    let uri = uri::get_uri(target);
     httping(
         target,
         format!(
@@ -290,7 +177,7 @@ pub fn httping_put(target: &str) -> Result<()> {
 }
 
 pub fn httping_delete(target: &str) -> Result<()> {
-    let uri = get_uri(target);
+    let uri = uri::get_uri(target);
     httping(
         target,
         format!(
@@ -305,7 +192,7 @@ pub fn httping_delete(target: &str) -> Result<()> {
     Ok(())
 }
 pub fn httping_patch(target: &str) -> Result<()> {
-    let uri = get_uri(target);
+    let uri = uri::get_uri(target);
     httping(
         target,
         format!(
